a359baf096f2dd537d9cc1076157dcd90ff8c427
Alex Manelis <amanelis@gmail.com>
2013-08-21 00:29:15 +0000

    Building out java package in chef

---
+++ cookbooks/platform/recipes/default.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook Name:: platform
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'monit'
+service 'monit' do
+  action [:enable, :start]
+end
+
+package 'ntp'
+template '/etc/ntp.conf' do
+  source    'ntp.conf.erb'
+  notifies  :restart, 'service[ntp]'
+end
+service 'ntp' do
+  action [:enable, :start]
+end
