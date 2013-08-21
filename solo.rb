7c64ef2f6c18b320c4df87333aa974fe24f255d7
Alex Manelis <amanelis@gmail.com>
2013-08-21 14:24:06 -0700

    Update solo.rb
    Changed the path for repo

--- solo.rb
+++ solo.rb
@@ -1,3 +1,3 @@
 file_cache_path "/root/chef-solo"
-cookbook_path "/root/chef-repo/cookbooks"
-data_bag_path "/root/chef-repo/data_bags"
+cookbook_path "/root/chef-agent/cookbooks"
+data_bag_path "/root/chef-agent/data_bags"
