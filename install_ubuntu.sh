6fd58237af913c9b15bcab94a09b30e762a6d8ae
Alex Manelis <amanelis@gmail.com>
2013-08-22 21:50:35 +0000

    Made some improvments on ubuntu bootstrap script

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -15,27 +15,31 @@ if ! test -f "$chef_binary"; then
 		adduser ubuntu root
 		adduser ubuntu sudo	
 
+		# System upgrades
+		apt-get update -y
+		apt-get upgrade -y
+
 		# Install Ruby
-		apt-get install ruby1.9.1 ruby1.9.1-dev rubygems1.9.1 irb1.9.1 ri1.9.1 rdoc1.9.1 -y 
-		apt-get install libopenssl-ruby1.9.1 libssl-dev zlib1g-dev -y
+		#apt-get install ruby1.9.1 ruby1.9.1-dev rubygems1.9.1 irb1.9.1 ri1.9.1 rdoc1.9.1 -y 
+		#apt-get install libopenssl-ruby1.9.1 libssl-dev zlib1g-dev -y
 
-		update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby1.9.1 400 \
-		--slave   /usr/share/man/man1/ruby.1.gz ruby.1.gz /usr/share/man/man1/ruby1.9.1.1.gz \
-		--slave   /usr/bin/ri ri /usr/bin/ri1.9.1 \
-		--slave   /usr/bin/irb irb /usr/bin/irb1.9.1 \
-		--slave   /usr/bin/rdoc rdoc /usr/bin/rdoc1.9.1
+		#update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby1.9.1 400 \
+		#--slave   /usr/share/man/man1/ruby.1.gz ruby.1.gz /usr/share/man/man1/ruby1.9.1.1.gz \
+		#--slave   /usr/bin/ri ri /usr/bin/ri1.9.1 \
+		#--slave   /usr/bin/irb irb /usr/bin/irb1.9.1 \
+		#--slave   /usr/bin/rdoc rdoc /usr/bin/rdoc1.9.1
 
-		update-alternatives --set ruby /usr/bin/ruby1.9.1
-		update-alternatives --set gem  /usr/bin/gem1.8
+		#update-alternatives --set ruby /usr/bin/ruby1.9.1
+		#update-alternatives --set gem  /usr/bin/gem1.8
     
+		# Install gems
+		#gem install chef ruby-shadow --no-ri --no-rdoc
+
 		# Install Chef
 		curl -L https://www.opscode.com/chef/install.sh | bash
 
 		# Clone our chef repository
-		git clone https://github.com/amanelis/chef-agent.git && cd chef-agent
-
-		# Install gems
-		gem install chef ruby-shadow --no-ri --no-rdoc
-	
+		git clone https://github.com/amanelis/chef-agent.git
 fi &&
+
 $chef_binary -c $chef_directory/solo.rb -j $chef_directory/platform_ubuntu.json
