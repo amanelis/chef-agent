2772c4173ae984762901c8676aba7c36ddc9ce9c
Alex Manelis <amanelis@gmail.com>
2013-08-22 21:29:46 +0000

    ntp was not working

--- cookbooks/platform/recipes/default.rb
+++ cookbooks/platform/recipes/default.rb
@@ -16,6 +16,10 @@ template '/etc/ntp.conf' do
   source    'ntp.conf.erb'
   notifies  :restart, 'service[ntp]'
 end
+template '/etc/init.d/ntp' do
+	source 		'ntp.erb'
+	notifies :restart, 'service[ntp]'
+end
 service 'ntp' do
   action [:enable, :start]
 end
