836d4fc46af95ce8a4b1e8fdd0273cedf9a01cee
Alex Manelis <amanelis@gmail.com>
2013-08-23 06:32:35 +0000

    Changed where rubygems are installed based on platform

--- cookbooks/platform/recipes/rubygems.rb
+++ cookbooks/platform/recipes/rubygems.rb
@@ -1,15 +1,23 @@
-# Install rubygems 
-remote_file "#{Chef::Config[:file_cache_path]}/rubygems-1.8.24.tgz" do
-	source "http://rubyforge.org/frs/download.php/76073/rubygems-1.8.24.tgz"
-	action :create_if_missing
-end
+case node[:platform]
+  when "debian", "ubuntu"
+  when "centos", "redhat", "amazon", "scientific"
+		# Install rubygems 
+		remote_file "#{Chef::Config[:file_cache_path]}/rubygems-1.8.24.tgz" do
+			source "http://rubyforge.org/frs/download.php/76073/rubygems-1.8.24.tgz"
+			action :create_if_missing
+		end
 
-bash "install_rubygems" do
-	cwd Chef::Config[:file_cache_path]
-	code <<-EOH
-		tar zxf rubygems-1.8.24.tgz
-		cd rubygems-1.8.24.tgz
-		/usr/bin/ruby setup.rb
-	EOH
-	action :nothing
-end
+		bash "install_rubygems" do
+			cwd Chef::Config[:file_cache_path]
+			code <<-EOH
+				tar zxf rubygems-1.8.24.tgz
+				cd rubygems-1.8.24
+				/usr/bin/ruby setup.rb
+			EOH
+			action :nothing
+		end  
+	end
+
+
+
+
