5c35bb94fe61c02f70984fccfa2d35be33e284b8
Alex Manelis <amanelis@gmail.com>
2013-08-23 22:40:11 +0000

    Small not_if

--- cookbooks/papertrail-rsyslog/recipes/log_files.rb
+++ cookbooks/papertrail-rsyslog/recipes/log_files.rb
@@ -6,13 +6,14 @@ end
 template '/etc/log_files.yml' do
 	source "log_files.yml.erb"
 	notifies :restart, 'service[remote_syslog]', :delayed
+	not_if { ::File.exists?('/etc/log_files.yml') }
 end
 
 bash "append_host_port_to_rsyslog" do
 	code <<-EOF
 		echo "*.*          @logs.papertrailapp.com:40007" >> /etc/rsyslog.conf
 	EOF
-	not_if { File.readlines('/etc/rsyslog.conf').grep(/logs.papertrailapp.com/).size > 0 }
+	not_if { ::File.readlines('/etc/rsyslog.conf').grep(/logs.papertrailapp.com/).size > 0 }
 	notifies :restart, "service[remote_syslog]", :delayed
 end
 
