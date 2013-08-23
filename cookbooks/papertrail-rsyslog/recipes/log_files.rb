a91842ef48142cb1ccd107c3089907481ac1548e
Alex Manelis <amanelis@gmail.com>
2013-08-23 22:41:48 +0000

    Fixing not_if logic on file exists?

--- cookbooks/papertrail-rsyslog/recipes/log_files.rb
+++ cookbooks/papertrail-rsyslog/recipes/log_files.rb
@@ -13,7 +13,7 @@ bash "append_host_port_to_rsyslog" do
 	code <<-EOF
 		echo "*.*          @logs.papertrailapp.com:40007" >> /etc/rsyslog.conf
 	EOF
-	not_if { ::File.readlines('/etc/rsyslog.conf').grep(/logs.papertrailapp.com/).size > 0 }
+	not_if { ::File.readlines('/etc/rsyslog.conf').grep(/logs.papertrailapp.com/).size == 1 }
 	notifies :restart, "service[remote_syslog]", :delayed
 end
 
