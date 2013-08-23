ede58f500904d56b89b5b4e67fa6bd5f7c859dfc
Alex Manelis <amanelis@gmail.com>
2013-08-23 19:22:50 +0000

    Wrong place for init file

--- cookbooks/papertrail-rsyslog/recipes/log_files.rb
+++ cookbooks/papertrail-rsyslog/recipes/log_files.rb
@@ -1,15 +1,14 @@
+template "/etc/init.d/remote_syslog" do
+	source "remote_syslog.erb"
+	mode "0775"		
+end
+
 bash "append_host_port_to_rsyslog" do
 	code <<-EOF
 		echo "*.*          @logs.papertrailapp.com:40007" >> /etc/rsyslog.conf
 	EOF
 	notifies :restart, "service[remote_syslog]", :immediately
 end
-
-template "/etc/init.d/remote_syslog" do
-	source "remote_syslog.erb"
-	mode "0775"		
-end
-
 
 service "remote_syslog" do
 	action [:enable, :start, :restart]
