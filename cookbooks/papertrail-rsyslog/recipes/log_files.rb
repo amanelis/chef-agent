53228a6f664559662b1835dbe087079cddcfd1ba
Alex Manelis <amanelis@gmail.com>
2013-08-23 22:43:47 +0000

    Trying comment

--- cookbooks/papertrail-rsyslog/recipes/log_files.rb
+++ cookbooks/papertrail-rsyslog/recipes/log_files.rb
@@ -9,13 +9,13 @@ template '/etc/log_files.yml' do
 	not_if { ::File.exists?('/etc/log_files.yml') }
 end
 
-bash "append_host_port_to_rsyslog" do
-	code <<-EOF
-		echo "*.*          @logs.papertrailapp.com:40007" >> /etc/rsyslog.conf
-	EOF
-	not_if { ::File.readlines('/etc/rsyslog.conf').grep(/logs.papertrailapp.com/).size == 1 }
-	notifies :restart, "service[remote_syslog]", :delayed
-end
+#bash "append_host_port_to_rsyslog" do
+#	code <<-EOF
+#		echo "*.*          @logs.papertrailapp.com:40007" >> /etc/rsyslog.conf
+#	EOF
+#	not_if { ::File.readlines('/etc/rsyslog.conf').grep(/logs.papertrailapp.com/).size == 1 }
+#	notifies :restart, "service[remote_syslog]", :delayed
+#end
 
 service "remote_syslog" do
 	action [:enable, :start]
