22546d4ec1597195d86df4d4f7d188c0e6c220fe
Alex Manelis <amanelis@gmail.com>
2013-08-26 15:51:17 +0000

    Changed owner on play

--- cookbooks/platform/recipes/play.rb
+++ cookbooks/platform/recipes/play.rb
@@ -1,11 +1,13 @@
 directory "/opt/play" do
 	mode "0755"
 	action :create
+	owner "play"
 end
 
 directory "/opt/play/releases" do
 	mode "0755"
 	action :create
+	owner "play"
 end
 
 execute "deployment" do
