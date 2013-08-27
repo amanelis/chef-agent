b7d475d87883c4505eb3127399423af750d491be
Alex Manelis <amanelis@gmail.com>
2013-08-27 17:59:51 +0000

    Updated the install bash

--- install_amzn1.sh
+++ install_amzn1.sh
@@ -21,21 +21,11 @@ if ! test -f "$chef_binary"; then
 		iconv-devel libtool	libffi-devel libyaml-devel make openssl openssl-devel patch readline readline-devel \
 		ruby-rdoc ruby-devel sqlite-devel svn zlib zlib-devel -y
 
-		# Install Ruby
-		#curl -L https://get.rvm.io | bash -s stable --ruby
-		#rm /usr/bin/ruby
-		#ln -s /home/ec2-user/.rvm/rubies/ruby-1.9.3-p362/bin/gem /usr/bin/gem
-		#ln -s /home/ec2-user/.rvm/rubies/ruby-1.9.3-p362/bin/ruby /usr/bin/ruby
-		#ln -s /home/ec2-user/.rvm/bin/rvm /usr/bin/rvm
-
 		# Install Chef
 		curl -L https://www.opscode.com/chef/install.sh | bash
 
 		# Clone our chef repository
 		git clone https://github.com/amanelis/chef-agent.git
-
-		# Install gems
-		# gem install chef ruby-shadow --no-ri --no-rdoc
 fi &&
 
 $chef_binary -c $chef_directory/solo.rb -j $chef_directory/platform_amzn1.json
