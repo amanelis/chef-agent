ca35746ecbed3343f838b1bf6fe0c5297ae528bf
Alex Manelis <amanelis@gmail.com>
2013-08-30 10:33:03 -0700

    Updated ENV aws key

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,2 +1,7 @@
+require 'yaml'
+
 cookbook_path [ '/root/chef-repo/cookbooks' ]
-node_name "localhost"
+node_name "signifyd"
+
+knife[:aws_access_key_id] 		= ENV['AWS_ACCESS_KEY_ID']
+knife[:aws_secret_access_key]	= ENV['AWS_SECRET_ACCESS_KEY']
