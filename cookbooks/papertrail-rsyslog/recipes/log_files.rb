fdf86a5ac23a39df853c1e8ebb77f571f502af80
Alex Manelis <amanelis@gmail.com>
2013-09-24 12:08:37 -0700

    Update the log_files

--- cookbooks/papertrail-rsyslog/recipes/log_files.rb
+++ cookbooks/papertrail-rsyslog/recipes/log_files.rb
@@ -6,12 +6,12 @@ end
 template '/etc/log_files.yml' do
 	source "log_files.yml.erb"
 	notifies :restart, 'service[remote_syslog]', :delayed
-	not_if { ::File.exists?('/etc/log_files.yml') }
+  # not_if { ::File.exists?('/etc/log_files.yml') }
 end
 
 bash "append_host_port_to_rsyslog" do
 	code <<-EOF
-		echo "*.*          @logs.papertrailapp.com:40007" >> /etc/rsyslog.conf
+		echo "*.* @logs.papertrailapp.com:40007" >> /etc/rsyslog.conf
 	EOF
 	not_if { ::File.readlines('/etc/rsyslog.conf').grep(/logs.papertrailapp.com/).size == 1 }
 	notifies :restart, "service[remote_syslog]", :delayed
