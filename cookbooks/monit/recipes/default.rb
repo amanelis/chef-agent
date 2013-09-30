be1ab519c7557e6b50fcb8bbdf886e4d60e92043
Alex Manelis <amanelis@gmail.com>
2013-09-30 14:23:06 -0700

    Updated how monit loads

--- cookbooks/monit/recipes/default.rb
+++ cookbooks/monit/recipes/default.rb
@@ -31,17 +31,17 @@ directory "/var/monit" do
   recursive true
 end
 
-template "/etc/monit.conf" do
-  owner "root"
-  group "root"
-  mode 0700
-  source 'monit.conf.erb'
-end
-
 template "/etc/monit/monitrc" do
   owner "root"
   group "root"
   mode 0700
   source 'monitrc.erb'
+end
+
+template "/etc/monit.conf" do
+  owner "root"
+  group "root"
+  mode 0700
+  source 'monit.conf.erb'
   notifies :restart, resources(:service => "monit"), :delayed
 end
