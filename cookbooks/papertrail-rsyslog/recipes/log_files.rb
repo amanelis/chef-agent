e2d2e25539ecc9e40da60eb18cd357f96b91606e
Alex Manelis <amanelis@gmail.com>
2013-10-02 00:08:47 -0700

    Turning off papertrail until deply_instance calls it

--- cookbooks/papertrail-rsyslog/recipes/log_files.rb
+++ cookbooks/papertrail-rsyslog/recipes/log_files.rb
@@ -5,7 +5,7 @@ end
 
 template '/etc/log_files.yml' do
 	source "log_files.yml.erb"
-	notifies :restart, 'service[remote_syslog]', :delayed
+  # notifies :restart, 'service[remote_syslog]', :delayed
 end
 
 bash "append_host_port_to_rsyslog" do
@@ -13,9 +13,9 @@ bash "append_host_port_to_rsyslog" do
 		echo "*.* @logs.papertrailapp.com:40007" >> /etc/rsyslog.conf
 	EOF
 	not_if { ::File.readlines('/etc/rsyslog.conf').grep(/logs.papertrailapp.com/).size == 1 }
-	notifies :restart, "service[remote_syslog]", :delayed
+  # notifies :restart, "service[remote_syslog]", :delayed
 end
 
-service "remote_syslog" do
-	action [:enable, :start, :restart]
-end
+# service "remote_syslog" do
+#   action [:enable, :start, :restart]
+# end
