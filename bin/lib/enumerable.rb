75b2ae9879468299e10d15bd01636fb193fa5b8f
Alex Manelis <amanelis@gmail.com>
2013-10-02 11:32:45 -0700

    Changed out loggers

--- bin/lib/enumerable.rb
+++ bin/lib/enumerable.rb
@@ -1,5 +1,6 @@
 # Class patching
-# Enumerable#in_parallel: allows you to parallelize work on collections.
+# Enumerable#in_parallel: allows you to parallelize work on collections. Remember, they
+# act like normal threads and do not recieve a copy of global variables.
 module Enumerable
   def in_parallel
     map { |x| Thread.new { yield(x) } }.each{ |t| t.join }
