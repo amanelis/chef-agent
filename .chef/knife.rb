04210faf128d30f17ccaffdacf7b8d6d0f3e1516
Alex Manelis <amanelis@gmail.com>
2014-07-30 10:25:09 -0700

    Changed name

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 
 cookbook_path [ '/root/chef-repo/cookbooks' ]
-node_name "signifyd"
+node_name "amanelis"
 
 knife[:aws_access_key_id] 		= ENV['AWS_ACCESS_KEY_ID']
 knife[:aws_secret_access_key]	= ENV['AWS_SECRET_ACCESS_KEY']
