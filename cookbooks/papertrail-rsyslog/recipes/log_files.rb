8a213dbdf6e9b91a236d4836dcb6ec15a77620e6
Alex Manelis <amanelis@gmail.com>
2014-07-30 10:32:35 -0700

    Remove port

--- cookbooks/papertrail-rsyslog/recipes/log_files.rb
+++ cookbooks/papertrail-rsyslog/recipes/log_files.rb
@@ -1,18 +1,18 @@
 template "/etc/init.d/remote_syslog" do
-	source "remote_syslog.erb"
-	mode "0775"		
+  source "remote_syslog.erb"
+  mode "0775"		
 end
 
 template '/etc/log_files.yml' do
-	source "log_files.yml.erb"
+  source "log_files.yml.erb"
   # notifies :restart, 'service[remote_syslog]', :delayed
 end
 
 bash "append_host_port_to_rsyslog" do
-	code <<-EOF
-		echo "*.* @logs.papertrailapp.com:40007" >> /etc/rsyslog.conf
-	EOF
-	not_if { ::File.readlines('/etc/rsyslog.conf').grep(/logs.papertrailapp.com/).size == 1 }
+  code <<-EOF
+    echo "*.* @logs.papertrailapp.com:CHANGEME" >> /etc/rsyslog.conf
+  EOF
+  not_if { ::File.readlines('/etc/rsyslog.conf').grep(/logs.papertrailapp.com/).size == 1 }
   # notifies :restart, "service[remote_syslog]", :delayed
 end
 
