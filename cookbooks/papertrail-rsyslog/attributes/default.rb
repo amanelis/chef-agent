770092de5f63c636b6c95e35a55f34ac51c63ac8
Alex Manelis <amanelis@gmail.com>
2014-07-30 10:34:27 -0700

    change me to port in attributes

--- cookbooks/papertrail-rsyslog/attributes/default.rb
+++ cookbooks/papertrail-rsyslog/attributes/default.rb
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-default['papertrail']['port'] = 40007
+default['papertrail']['port'] = "CHANGE ME"
 default['papertrail']['resume_retry_count'] = -1
 default['papertrail']['syslog_selector'] = '*.*'
 default['papertrail']['queue_disk_space'] = '100M'
